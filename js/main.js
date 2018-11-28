3e7bfcab7d3694e0da0ad88cb5763e0e547c800d
NMadmin <spam.block.plus@gmail.com>
2018-11-27 19:45:54 -0500

    gallery addition

--- js/main.js
+++ js/main.js
@@ -52,11 +52,22 @@ function animateElement(element, animation, timing){
 }
 // Custom Animation Functions
 function animateContact (){
-	if($("iframe")[0].style.display == "block")
+	log($('.Velocity-animating'))
+	// if($('.Velocity-animating'))
+	// {
+	// 	Velocity('stop')
+	// }
+	if($(".swiper1")[0].style.display == "block" )
+	{
+		animateGallery()
+	}
+	if($("iframe")[0].style.display == "block" )
 	{
 		fastdom.mutate(function(){
 		animateElement($("iframe"),{opacity: '0.0'}, 400)
-		setTimeout(function(){animateElement($("iframe"),{display: 'none'}, 100) },400)
+		setTimeout(function(){
+			animateElement($("iframe"),{display: 'none'}, 100) 
+		},400)
 	})
 	}
 	else
@@ -66,8 +77,54 @@ function animateContact (){
 	})
 	}
 }
+function animateGallery(){
 
-// animateElement($(".button"),fadeOut,4000)
+	if($("iframe")[0].style.display == "block")
+	{
+		animateContact()
+	}
+	log($(".swiper1")[0].style.display)
+		if($(".swiper1")[0].style.display == "block")
+		 {
+			fastdom.mutate(function(){
+				animateElement($(".img-gal"),{opacity: '0.0'}, 400)
+				setTimeout(function(){
+					animateElement($(".swiper1"),{display: 'none'}, 100) 
+					animateElement($(".swiper2"),{display: 'none'}, 100) 
+				},400)
+				setTimeout(function(){
+					animateElement($(".img-gal"),{height: '0px'}, 100) 
+				},400)
+
+			})
+		}
+		else 
+		{
+			fastdom.mutate(function(){		
+				animateElement($(".swiper1"),{display: 'block'}, 100)
+				animateElement($(".swiper2"),{display: 'block'}, 100)
+				setTimeout(function(){
+					animateElement($(".img-gal"),{height: '1120px'}, 100) 
+				},400)
+				setTimeout(function(){
+					animateElement($(".img-gal"),{opacity: '1.0'}, 240) 
+				},100)
+			})
+		}
+ }
+// Variable Setup
+var swiper1 = new Swiper('.swiper1', {
+	observer: true
+})
+var swiper1 = new Swiper('.swiper2', {
+	observer: true
+})
+
+
+
+// Listeners
+document.getElementById("contact_btn").addEventListener("click", animateContact);
+document.getElementById("port_btn").addEventListener("click", animateGallery);
 
 //Init Animations
 fastdom.mutate(function(){
@@ -80,6 +137,3 @@ setTimeout(function(){
 	fastdom.mutate(function(){
 		animateElement($(".btn"),{opacity: '1.0'}, 2400)
 	}) }, 4400);
-
-// Listeners
-document.getElementById("contact_btn").addEventListener("click", animateContact);
