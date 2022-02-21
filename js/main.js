ae527ff2f82342a75ebcaf6edd450552df35597a
NMandes <NMandes@users.noreply.github.com>
2022-02-21 01:56:51 -0500

    updated swiper

--- js/main.js
+++ js/main.js
@@ -24,26 +24,26 @@ function $(element_id){
 		console.log("ERROR: element_id is not a string")
 	}
 }
-function _(element_id){
-	var ele_obj = {}
-	fastdom.measure(function(){
-			if (typeof element_id == "string")
-			{
-				if (element_id[0] == "." || element_id[0] == "#")
-				{
-					var style = window.getComputedStyle(document.querySelector(element_id))
-					ele_obj.width = style.getPropertyValue('width')
-					ele_obj.height =  style.getPropertyValue('height')
-					ele_obj.opacity = style.getPropertyValue('opacity')
-				}
-			}
-			else
-			{
-				console.log("ERROR: element_id is not a string")
-			}
-	})
-	return ele_obj
-}
+// function _(element_id){
+// 	var ele_obj = {}
+// 	fastdom.measure(function(){
+// 			if (typeof element_id == "string")
+// 			{
+// 				if (element_id[0] == "." || element_id[0] == "#")
+// 				{
+// 					var style = window.getComputedStyle(document.querySelector(element_id))
+// 					ele_obj.width = style.getPropertyValue('width')
+// 					ele_obj.height =  style.getPropertyValue('height')
+// 					ele_obj.opacity = style.getPropertyValue('opacity')
+// 				}
+// 			}
+// 			else
+// 			{
+// 				console.log("ERROR: element_id is not a string")
+// 			}
+// 	})
+// 	return ele_obj
+// }
 function animateElement(element, animation, timing){
 			Velocity(element, animation, timing)
 }
@@ -52,65 +52,91 @@ function animateElement(element, animation, timing){
 // Variable Setup
 var swiper1 = new Swiper('.swiper1', {
 	observer: true,
+	grabCursor: true,
+	touchMoveStopPropagation: true,
 	autoplay: {
-    delay: 5000,
+    delay: 5000
   },
   zoom: {
-    toggle: false
+    toggle: true
   },
+  pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+  }
 
 })
-var swiper1 = new Swiper('.swiper2', {
+var swiper2 = new Swiper('.swiper2', {
 	observer: true,
+	grabCursor: true,
 	autoplay: {
-    delay: 5000,
+    delay: 5000
   },
   zoom: {
-    toggle: false
+    toggle: true
+  },
+  pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
   }
+
 })
-var swiper1 = new Swiper('.swiper3', {
+var swiper3 = new Swiper('.swiper3', {
 	observer: true,
+	grabCursor: true,
 	autoplay: {
-    delay: 5000,
+    delay: 5000
   },
   zoom: {
-    toggle: false
+    toggle: true
+  },
+  pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
   }
+
 })
-var swiper1 = new Swiper('.swiper4', {
+var swiper4 = new Swiper('.swiper4', {
 	observer: true,
+	grabCursor: true,
 	autoplay: {
-    delay: 5000,
+    delay: 5000
   },
   zoom: {
-    toggle: false
+    toggle: true
+  },
+  pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
   }
+
 })
 
 // Listeners
 
 //Init Animations
 fastdom.mutate(function(){
-animateElement($("footer"),{backgroundColor: '#222222', opacity: '1.0'},4400)
-animateElement($("body"),{backgroundColor: '#DDDDDD'},4400)
-animateElement($(".initials"),{color:'#ffffff'},4000)
-animateElement($("#name"),{color: '#222222', letterSpacing: '9px', opacity: '0.8'},4400)
-animateElement($("#subhead"),{color: '#444444', opacity: '0.7'},5000)
+
+animateElement($("body"), {backgroundColor: '#DDDDDD'}, 4400)
+animateElement($(".initials"), {color:'#ffffff'}, 4000)
+animateElement($("#name"), {color: '#222222', letterSpacing: '9px', opacity: '0.8'}, 4400)
+animateElement($("#subhead"), {color: '#444444', opacity: '0.7'}, 5000)
 })
 setTimeout(function(){
 	fastdom.mutate(function(){
-				animateElement($(".swiper1"),{display: 'block'}, 100)
-				animateElement($(".swiper2"),{display: 'block'}, 100)
-				animateElement($(".swiper3"),{display: 'block'}, 100)
-				animateElement($(".swiper4"),{display: 'block'}, 100)
+
+				animateElement($(".swiper1"), {display: 'block'}, 100)
+				animateElement($(".swiper2"), {display: 'block'}, 100)
+				animateElement($(".swiper3"), {display: 'block'}, 100)
+				animateElement($(".swiper4"), {display: 'block'}, 100)
+				animateElement($("footer"), {backgroundColor: '#222222', opacity: '1.0'}, 140)
 
 				setTimeout(function(){
-					animateElement($(".img-gal"),{height: '100%'}, 100)
-				},400)
+					animateElement($(".img-gal"), {height: '100%'}, 100)
+				}, 400)
 				setTimeout(function(){
-					animateElement($(".img-gal"),{opacity: '1.0'}, 240)
-				},100)
+					animateElement($(".img-gal"), {opacity: '1.0'}, 240)
+				}, 100)
 
 
 	}) }, 4500);
